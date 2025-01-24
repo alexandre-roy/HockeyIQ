ebd53fc6f1e0127237287149b2a3021a2ac556bd
Alexandre Roy <alexandre15roy@gmail.com>
2025-01-24 17:04:55 -0500

    Début du fichier pour extraire les données

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt6.QtGui import QFont
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -7,11 +8,15 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("HockeyIQ")
         self.setGeometry(325, 225, 800, 500)
 
+        label = QLabel("Hello World", self)
+        label.setFont(QFont("JetBrains Mono", 12))
+        label.margin()
+
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    sys .exit(app.exec_())
+    sys .exit(app.exec())
 
 if __name__ == "__main__":
     main()
