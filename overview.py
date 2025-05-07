7920567412ed42e9263a789f1bce606eb9e4c05d
Alexandre Roy <alexandre15roy@gmail.com>
2025-05-07 10:22:12 -0400

    FEAT (video) - Ajout du popup pour le vidéo tutoriel

--- overview.py
+++ overview.py
@@ -1,11 +1,37 @@
 #pylint: disable = no-name-in-module
 
 """Modules"""
-from PyQt6.QtWidgets import QLabel, QWidget, QListWidget, QListWidgetItem, QFrame, QVBoxLayout, QHBoxLayout
+from PyQt6.QtWidgets import QLabel, QWidget, QListWidget, QListWidgetItem, QFrame, QVBoxLayout, QHBoxLayout, QPushButton, QDialog
 from PyQt6.QtGui import QFontDatabase, QFont, QCursor
-from PyQt6.QtCore import Qt
+from PyQt6.QtMultimedia import QMediaPlayer, QAudioOutput
+from PyQt6.QtMultimediaWidgets import QVideoWidget
+from PyQt6.QtCore import Qt, QUrl
 import bd
 import utils
+
+class VideoPopup(QDialog):
+    """Popup window to play the video"""
+    def __init__(self, video_path, parent=None):
+        super().__init__(parent)
+        self.setGeometry(435, 300, 600, 400)
+
+        self.border_label = QLabel(self)
+        self.border_label.setGeometry(0,0,0,0)
+        self.border_label.setStyleSheet("background-color: #f2bd41;")
+
+        self.media_player = QMediaPlayer(self)
+        self.audio_output = QAudioOutput(self)
+        self.media_player.setAudioOutput(self.audio_output)
+
+        self.video_widget = QVideoWidget(self)
+        self.video_widget.setGeometry(30, 30, 555, 365)
+        self.media_player.setVideoOutput(self.video_widget)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.video_widget)
+
+        self.media_player.setSource(QUrl.fromLocalFile(video_path))
+        self.media_player.play()
 
 class Overview(QWidget):
     """Page d'overview"""
@@ -36,6 +62,25 @@ class Overview(QWidget):
         label_sommaire_bg.setFont(self.jersey25_64)
         label_sommaire_bg.setStyleSheet("color: #2f3038")
 
+        self.video_button_bg_1 = QPushButton(self)
+        self.video_button_bg_1.setGeometry(530, 21, 120, 43)
+        self.video_button_bg_1.setStyleSheet("background-color: #2f3038; border-radius: 0px;")
+
+        self.video_button_bg_2 = QPushButton(self)
+        self.video_button_bg_2.setGeometry(527, 24, 126, 37)
+        self.video_button_bg_2.setStyleSheet("background-color: #2f3038; border-radius: 0px;")
+
+        self.video_button_fg_1 = QPushButton(self)
+        self.video_button_fg_1.setGeometry(533, 18, 120, 43)
+        self.video_button_fg_1.setStyleSheet("background-color: #62a1a6; border-radius: 0px;")
+
+        self.video_button_fg_2 = QPushButton(self)
+        self.video_button_fg_2.setGeometry(530, 21, 126, 37)
+        self.video_button_fg_2.setStyleSheet("background-color: #62a1a6; border-radius: 0px;")
+        self.video_button_fg_2.clicked.connect(self.open_video_popup)
+        self.video_button_fg_2.setFont(self.jersey25_32)
+        self.video_button_fg_2.setText("Tutoriel")
+        self.video_button_fg_2.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
 
         label_sommaire = QLabel(self)
         label_sommaire.setGeometry(22, 18, 300, 40)
@@ -81,6 +126,12 @@ class Overview(QWidget):
         self.list_joueurs.setStyleSheet("""background-color: #bbbcc0""")
 
         self.populer_listes(categorie, self.list_equipes, self.list_joueurs)
+
+    def open_video_popup(self):
+        """Ouvre le vidéo"""
+        chemin = "Resources/Videos/tutoriel.mov"
+        video_popup = VideoPopup(chemin, self)
+        video_popup.exec()
 
     def get_equipes(self, categorie):
         """Récupère les équipes dans la base de données"""
